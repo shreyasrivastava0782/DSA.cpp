6aef1ebd68c11e4f0aa71466b128b0e51dcdf651
Shreya Srivastava <shreyasrivastava722@gmail.com>
2026-01-18 00:54:01 +0530

    Time: 48 ms (36.84%), Space: 55.6 MB (6.1%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,16 @@
+/**
+ * @param {any} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(other) {
+            if (val === other) return true;
+            throw new Error("Not Equal");
+        },
+        notToBe: function(other) {
+            if (val !== other) return true;
+            throw new Error("Equal");
+        }
+    };
+};
